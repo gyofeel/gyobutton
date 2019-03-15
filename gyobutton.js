f298d75b43d54e1879a46adfde06dfc277237c6e
서교필 <gyofeel318@gmail.com>
2019-03-15 17:10:03 +0900

    add callback parameters to return toggle element node and toggle state boolean value & fix styleObj valuable inheritance problems.

--- gyobutton.js
+++ gyobutton.js
@@ -31,9 +31,9 @@
     const returnComputedStyle = (node ,property)=>{
         return window.getComputedStyle(node)[property];
     };
-    const initStyle = (node)=>{
-        const initStyle = Object.entries(styleObj.init);
-        for(let v of initStyle){
+    const initStyle = (node, style)=>{
+        const iStyle = Object.entries(style);
+        for(let v of iStyle){
             node.style[v[0]] = v[1];
         }
     };
@@ -42,7 +42,7 @@
             e.stopPropagation();
             callback();
         });
-    }
+    };
 
     //Constructor
     const GyoButton = function(sel){
@@ -66,9 +66,9 @@
 
     GyoButton.prototype = {
         button : (initStyleProperty)=>{
-            styleObj.init = (initStyleProperty)?initStyleProperty:styleObj.init;
+            const style = (initStyleProperty)?initStyleProperty:styleObj.init;
             for(let node of nodeArr){
-                initStyle(node);
+                initStyle(node, style);
             }
         },
         event : (eventName, callback)=>{
@@ -76,29 +76,31 @@
                 initEvent(node, eventName, callback);
             }
         }
-    }
+    };
 
     //GyoToggleButton
     //Private Member
     let nodeStateArr;
     //Method
-    const initToggle = ()=>{
+    const initToggle = (cb, effect, effectOut)=>{
         for(let el of nodeStateArr){
              el[0].addEventListener('mouseup', (e)=>{
                 e.stopPropagation();
                 el[1] = !el[1];
                 let stateStyle;
                 if(el[1]) {
-                    stateStyle = Object.entries(styleObj.state_effect);
+                    stateStyle = Object.entries(effect);
                 } else{
-                    stateStyle = Object.entries(styleObj.state_effect_out);
+                    stateStyle = Object.entries(effectOut);
                 }
                 for(let v of stateStyle){
                     e.currentTarget.style[v[0]] = v[1];
                 }
+                //callback function have value of node element and toggle state of this.
+                cb(e.currentTarget, el[1]);
              })
         }
-    }
+    };
     const GyoToggleButton = function(sel){
         GyoButton.call(this, sel);
         nodeStateArr = Array.from(nodeArr).map((el)=>[el, false]);
@@ -106,11 +108,11 @@
     GyoToggleButton.prototype = Object.create(GyoButton.prototype);
     GyoToggleButton.prototype.constructor = GyoToggleButton;
 
-    GyoToggleButton.prototype.toggle = (stateProp, stateOutProp)=>{
-        styleObj.state_effect = (stateProp)?stateProp:styleObj.state_effect;
-        styleObj.state_effect_out = (stateOutProp)?stateOutProp:styleObj.state_effect_out;
-        initToggle();
-    }
+    GyoToggleButton.prototype.toggle = (callback, stateProp, stateOutProp)=>{
+        const styleStateEffect = (stateProp)?stateProp:styleObj.state_effect;
+        const styleStateEffectOut = (stateOutProp)?stateOutProp:styleObj.state_effect_out;
+        initToggle(callback, styleStateEffect, styleStateEffectOut);
+    };
 
    window.gyoButton = GyoButton;
    window.gyoToggleButton = GyoToggleButton;
