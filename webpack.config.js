56a7197b6ccd50d1403bd216109418657b8e64dd
서교필 <gyofeel318@gmail.com>
2019-03-19 11:08:08 +0900

    change the name of the bundled(bundle.js -> gyobutton.js)

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
     output: {
         path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        filename: 'gyobutton.js'
     },
     module:{
         rules:[
