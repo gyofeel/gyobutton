acc279f66c63c64fb3e71f64de12a876c5083b9f
서교필 <gyofeel318@gmail.com>
2019-03-20 15:19:02 +0900

    add Gyo.noConflict() to prevent conflict with external library because of alias keyword.

--- src/js/gyo.js
+++ src/js/gyo.js
@@ -1,8 +1,11 @@
 import GyoButton from './button';
 import GyoToggleButton from './togglebutton';
+import {noConflict} from './methods.js'
 
-let Gyo 
-window.Gyo = Gyo = {};
+let Gyo;
+window.Gyo = Gyo = Gyo || {};
 
 Gyo.button = GyoButton;
 Gyo.toggleButton = GyoToggleButton;
+
+Gyo.noConflict = noConflict;
---
+++ src/js/methods.js
@@ -0,0 +1,10 @@
+let _Gyo = window.Gyo;
+const noConflict = (deep)=>{
+    let temp = window.Gyo;
+    if(deep&&window.Gyo === Gyo){
+        window.Gyo = _Gyo;
+    }
+    return temp;
+}
+
+export {noConflict};
